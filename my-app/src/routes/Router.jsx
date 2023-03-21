2deecb0966a525f3b4148842da8a931355fc66a5
takeso10 <sotatake7716@gmail.com>
2023-03-21 13:16:36 +0900

    書籍レビュー完成

--- my-app/src/routes/Router.jsx
+++ my-app/src/routes/Router.jsx
@@ -18,20 +18,20 @@ export const Router = () => {
           <Routes>
             {auth ?(
               <>
-                <Route path="React.js3" element={<Home />} />
-                <Route path="React.js3/signup" element={<Navigate replace to="/"/>} />
-                <Route path="React.js3/login" element={<Navigate replace to="/"/>} />
-                <Route path='React.js3/profile' element={<Profile/>}/>
-                <Route path='React.js3/new' element={<NewReview/>}/>
-                <Route path='React.js3/detail/:id' element={<ReviewDetail/>}/>
-                <Route path='React.js3/edit/:id' element={<EditReview/>}/>
+                <Route path="/React.js3" element={<Home />} />
+                <Route path="/React.js3/signup" element={<Navigate replace to="/"/>} />
+                <Route path="/React.js3/login" element={<Navigate replace to="/"/>} />
+                <Route path='/React.js3/profile' element={<Profile/>}/>
+                <Route path='/React.js3/new' element={<NewReview/>}/>
+                <Route path='/React.js3/detail/:id' element={<ReviewDetail/>}/>
+                <Route path='/React.js3/edit/:id' element={<EditReview/>}/>
               </>
             ):(
               <>
-                <Route path="React.js3" element={<Navigate replace to="/login" />} />
-                <Route path="React.js3/login" element={<Login />} />
-                <Route path="React.js3/signup" element={<SignUp />} />
-                <Route path='React.js3/profile' element={<Navigate replace to="/"/>}/>
+                <Route path="/React.js3" element={<Navigate replace to="/login" />} />
+                <Route path="/React.js3/login" element={<Login />} />
+                <Route path="/React.js3/signup" element={<SignUp />} />
+                <Route path='/React.js3/profile' element={<Navigate replace to="/"/>}/>
               </>
             )}
             <Route component={NotFound} />
